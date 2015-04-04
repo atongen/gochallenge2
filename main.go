2d8c587b04727360a2e95d76cc44d9a9ddf3e081
Andrew Tongen <atongen@gmail.com>
2015-04-04 10:40:47 -0500

    Tests passing

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"crypto/rand"
 	"flag"
 	"fmt"
@@ -21,7 +20,7 @@ import (
 
 type SecureReader struct {
 	r         io.Reader
-	priv, pub *[32]byte
+	sharedKey *[32]byte
 }
 
 func (r SecureReader) Read(p []byte) (int, error) {
@@ -38,10 +37,11 @@ func (r SecureReader) Read(p []byte) (int, error) {
 	var nonce [24]byte
 	copy(nonce[:], message[:24])
 
-	decrypted, ok := box.Open([]byte{}, message[24:], &nonce, r.pub, r.priv)
+	decrypted, ok := box.OpenAfterPrecomputation([]byte{}, message[24:], &nonce, r.sharedKey)
 	if !ok {
 		log.Fatalln("unable to open box")
 	}
+	//println("read:", string(decrypted))
 	copy(p, decrypted)
 
 	return len(decrypted), nil
@@ -49,12 +49,14 @@ func (r SecureReader) Read(p []byte) (int, error) {
 
 // NewSecureReader instantiates a new SecureReader
 func NewSecureReader(r io.Reader, priv, pub *[32]byte) io.Reader {
-	return SecureReader{r, priv, pub}
+	var sharedKey [32]byte
+	box.Precompute(&sharedKey, pub, priv)
+	return SecureReader{r, &sharedKey}
 }
 
 type SecureWriter struct {
 	w         io.Writer
-	priv, pub *[32]byte
+	sharedKey *[32]byte
 }
 
 func (w SecureWriter) Write(p []byte) (int, error) {
@@ -62,8 +64,9 @@ func (w SecureWriter) Write(p []byte) (int, error) {
 	if _, err := rand.Read(nonce[:]); err != nil {
 		return 0, err
 	}
+	//println("write:", string(p))
 
-	encrypted := box.Seal(nonce[:], p, &nonce, w.pub, w.priv)
+	encrypted := box.SealAfterPrecomputation(nonce[:], p, &nonce, w.sharedKey)
 	w.w.Write(encrypted)
 
 	return len(encrypted), nil
@@ -71,13 +74,22 @@ func (w SecureWriter) Write(p []byte) (int, error) {
 
 // NewSecureWriter instantiates a new SecureWriter
 func NewSecureWriter(w io.Writer, priv, pub *[32]byte) io.Writer {
-	return SecureWriter{w, priv, pub}
+	var sharedKey [32]byte
+	box.Precompute(&sharedKey, pub, priv)
+	return SecureWriter{w, &sharedKey}
 }
 
 type SecureReadWriteCloser struct {
 	sr   io.Reader
 	sw   io.Writer
 	conn net.Conn
+}
+
+func NewSecureReadWriteCloser(conn net.Conn, priv, pub *[32]byte) io.ReadWriteCloser {
+	sr := NewSecureReader(conn, priv, pub)
+	sw := NewSecureWriter(conn, priv, pub)
+
+	return &SecureReadWriteCloser{sr, sw, conn}
 }
 
 func (srwc SecureReadWriteCloser) Read(p []byte) (int, error) {
@@ -96,48 +108,134 @@ func (srwc SecureReadWriteCloser) Close() error {
 // connects to the server, perform the handshake
 // and return a reader/writer.
 func Dial(addr string) (io.ReadWriteCloser, error) {
-	pub, priv, err := box.GenerateKey(rand.Reader)
+	myPub, myPriv, err := box.GenerateKey(rand.Reader)
 	if err != nil {
-		log.Fatalln("error generating keys", err)
+		log.Println("error generating client keys", err)
+		return nil, err
 	}
 
 	conn, err := net.Dial("tcp", addr)
 	if err != nil {
-		log.Fatalln("error dialing server", err)
+		log.Println("error dialing server", err)
+		return nil, err
 	}
-	sr := NewSecureReader(conn, priv, pub)
-	sw := NewSecureWriter(conn, priv, pub)
 
-	srwc := SecureReadWriteCloser{sr, sw, conn}
+	peerPub, err := clientHandshake(conn, myPub)
+	if err != nil {
+		log.Println("client handshake failed", err)
+		return nil, err
+	}
+
+	srwc := NewSecureReadWriteCloser(conn, myPriv, peerPub)
 
 	return srwc, nil
+}
+
+func clientHandshake(conn net.Conn, pub *[32]byte) (*[32]byte, error) {
+	// write my pub key to connection
+	_, err := fmt.Fprintf(conn, string(pub[:32]))
+	if err != nil {
+		return nil, err
+	}
+
+	// read server pub key from connection
+	buf := make([]byte, 1024)
+	_, err = conn.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	var peerPub [32]byte
+	copy(peerPub[:], buf[0:32])
+	return &peerPub, nil
+}
+
+func serverHandshake(conn net.Conn, pub *[32]byte) (*[32]byte, error) {
+	// read client pub key from connection
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	// write my pub key to connection
+	_, err = fmt.Fprintf(conn, string(pub[:32]))
+	if err != nil {
+		return nil, err
+	}
+
+	var peerPub [32]byte
+	copy(peerPub[:], buf[0:32])
+	return &peerPub, nil
+}
+
+// Create new connection from rwc.
+func newConn(rwc net.Conn, priv, pub *[32]byte) *conn {
+	c := new(conn)
+	c.remoteAddr = rwc.RemoteAddr().String()
+	c.rwc = rwc
+	c.srwc = NewSecureReadWriteCloser(rwc, priv, pub)
+	return c
+}
+
+// A conn represents the server side of an HTTP connection.
+type conn struct {
+	remoteAddr string   // network address of remote side
+	rwc        net.Conn // i/o connection
+	srwc       io.ReadWriteCloser
 }
 
 // Serve starts a secure echo server on the given listener.
 // http://golang.org/src/net/http/server.go?s=51504:51550#L1714
 // http://loige.co/simple-echo-server-written-in-go-dockerized/
 func Serve(l net.Listener) error {
+	myPub, myPriv, err := box.GenerateKey(rand.Reader)
+	if err != nil {
+		return err
+	}
+
 	for {
 		conn, err := l.Accept()
 		if err != nil {
-			log.Fatalln("error accepting connection", err)
+			continue
 		}
-		go handleRequest(conn)
+		peerPub, err := serverHandshake(conn, myPub)
+		if err != nil {
+			continue
+		}
+		c := newConn(conn, myPriv, peerPub)
+		go c.serve()
 	}
+
+	return err
 }
 
-func handleRequest(conn net.Conn) {
-	buf := make([]byte, 1024)
-	// Read the incoming connection into the buffer.
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Fatalln("error reading request", err)
+// Serve a new connection.
+func (c *conn) serve() {
+	for {
+		// read from the client connection
+		buf := make([]byte, 2048)
+		n, err := c.srwc.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				//println("conn read EOF")
+				break // Don't reply
+			} else if neterr, ok := err.(net.Error); ok && neterr.Timeout() {
+				//println("conn read timeout")
+				break // Don't reply
+			} else {
+				//println("conn read failed: ", err)
+				break
+			}
+		}
+
+		// write to the client connection
+		_, err = fmt.Fprintf(c.srwc, string(buf[:n]))
+		if err != nil {
+			println("conn write failed", err)
+			break
+		}
 	}
-
-	n := bytes.Index(buf, []byte{0})
-
-	conn.Write(buf[:n])
-	conn.Close()
 }
 
 func main() {
